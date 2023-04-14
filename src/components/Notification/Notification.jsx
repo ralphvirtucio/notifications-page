d52e1d0c4e21ae2090d6e66dbecf81edbf5aa92e
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-04-15 00:14:41 +0800

    WIP: Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import mark from '../../images/avatar-mark-webber.webp';
 
 export const Notification = () => {
   return (
     <div>
-      <h2>Mark Webber</h2>
+      <img src={mark} alt='Mark Webber' />
+      <div className='content'>
+        <h2>Mark Webber</h2>
+        <p>reacted to your recent post My first tournament!</p>
+        <p>1m ago</p>
+      </div>
     </div>
   );
 };
+
+//   Mark Webber reacted to your recent post My first tournament today!
+//   1m ago
