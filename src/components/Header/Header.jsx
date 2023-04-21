68d9df652a1532a0841927281604da925bd9cb1e
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-04-21 22:41:39 +0800

    Updates on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,16 @@ export const Header = () => {
     <header className={styles.container}>
       <div className={styles['notification__heading-badge']}>
         <h1 className={styles.notification__heading}>Notifications</h1>
-        <p className={styles.notification__pill}>3</p>
+
+        <p className={styles.notification__pill}>
+          3{/* This will be a state */}
+        </p>
       </div>
 
       <div>
         <button className={styles['notification__mark-btn']}>
           Mark all as read
+          {/* Button responsible for marking all the notification as read */}
         </button>
       </div>
     </header>
